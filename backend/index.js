5d7e3e6a73f9515be46dee7b4b519cd18cc73258
kofman10 <kajotonis@gmal.com>
2022-12-21 03:32:17 -0800

    first commit

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require("cors");
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+
+// Enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/openai', require('./Routes/openairoute'));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
